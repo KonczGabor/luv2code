811207c6ab3fe7d213eb37391fb388e220e1ba30
Calindyl <caridniss@gmail.com>
2019-04-01 00:12:56 +0200

    First commit

---
+++ src/main/java/gg/demo/Luv2codeApplication.java
@@ -0,0 +1,13 @@
+package gg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luv2codeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Luv2codeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/gg/demo/controller/HomeController.java
@@ -0,0 +1,5 @@
+package gg.demo.controller;
+
+public class HomeController {
+
+}
